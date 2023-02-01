c31375ecaceee0fba95672660b22193829dbb184
akida15 <96668437+akida15@users.noreply.github.com>
2023-02-01 23:56:06 +0100

    Update photo.js

--- commands/photo.js
+++ commands/photo.js
@@ -1,36 +1,47 @@
-const { addnote,cmd, sck1, delnote, allnotes, delallnote, tlang, botpic, runtime, prefix, Config } = require('../lib')
-    //---------------------------------------------------------------------------//
-    function akidapic() {	
-        return new Promise( (resolve, reject) => {	
-          let LangG = getString("global");	
-          let todlink = [
-            "https://www.entoin.com/images/sllaye19.jpg",
-            "https://www.entoin.com/images/sllaye20.jpg",
-            "https://www.entoin.com/images/sllaye21.jpg"
-        ];	
-       const picsecktorh = todlink[Math.floor(Math.random() * todlink.length)];	
-       resolve(picsecktorh)	
-       })	
-        }	
-    //---------------------------------------------------------------------------//
+/**
+ Copyright (C) 2022.
+ Licensed under the  GPL-3.0 License;
+ You may not use this file except in compliance with the License.
+ It is supplied in the hope that it may be useful.
+ * @project_name : Secktor-Md
+ * @author : SamPandey001 <https://github.com/SamPandey001>
+ * @description : Secktor,A Multi-functional whatsapp bot.
+ * @version 0.0.6
+ **/
+
+ const { addnote,cmd, sck1, delnote, allnotes, delallnote, tlang, botpic, runtime, prefix, Config } = require('../lib')
+ //---------------------------------------------------------------------------
+ function akida() {	
+    return new Promise( (resolve, reject) => {	
+      let LangG = getString("akida");	
+      let todlink = [`${LangG.pic1}`,`${LangG.pic2}`,`${LangG.pic3}`,`${LangG.pic4}`,`${LangG.pic5}`,`${LangG.pic6}`	
+     ];	
+   const picsecktorh = todlink[Math.floor(Math.random() * todlink.length)];	
+   resolve(picsecktorh)	
+   })	
+    }	
+
+    //---------------------------------------------------------------------------
 
 cmd({
-        pattern: "احزر",
-        filename: __filename,
-    },
-    async(Void, citel, text, isAdmins) => {
-        const alivtxt = `احزر اسم الشخصية`;
-        let aliveMessage = {
-            image: {
-                url: await akidapic(),
-            },
-            caption: alivtxt,
-            footer: tlang().footer,
-            headerType: 4,
-        };
-         return Void.sendMessage(citel.chat, aliveMessage, {
-            quoted: citel,
-        });
+         pattern: "احزر",
+         category: "",
+         filename: __filename,
+         desc: ""
+     },
+     async(Void, citel, text, isAdmins) => {
+         const alivtxt = `احزر اسم الشخصية`;
+         let aliveMessage = {
+             image: {
+                 url: await akida(),
+             },
+             caption: alivtxt,
+             footer: tlang().footer,
+             headerType: 4,
+         };
+          return Void.sendMessage(citel.chat, aliveMessage, {
+             quoted: citel,
+         });
 
-    }
-)
+     }
+ )
