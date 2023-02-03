8c5f05d86d54758e6fbebd60454a8dbbd487f691
akida15 <96668437+akida15@users.noreply.github.com>
2023-02-03 02:51:38 +0100

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -3,8 +3,9 @@ const axios = require('axios')
 const speed = require('performance-now')
 //---------------------------------------------------------------------------
 
-const email = 'botbotawa@gmail.com';
-const password = 'nourajarifi';
+const axios = require('axios');
+
+const API_KEY = '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';
 
 cmd({
   pattern: "chat",
@@ -12,24 +13,19 @@ cmd({
   category: "General",
   use: '<Hii,Secktor>',
   filename: __filename,
-}, async(Void, citel, text) => {
-  if (!text) {
-    return citel.reply(`Hey there! ${citel.pushName}. How are you doing these days?`);
-  }
-
+},
+async (Void, citel, text) => {
   axios.post('https://chat.openai.com/chat', {
-    prompt: text,
+    prompt: text || 'Hello',
   }, {
-    auth: {
-      username: email,
-      password: password,
+    headers: {
+      'Authorization': `Bearer ${API_KEY}`,
     },
   })
   .then(response => {
-    citel.reply(response.data);
+    citel.reply(response.data.choices[0].text);
   })
   .catch(error => {
     console.error(error);
-    citel.reply(`An error occurred: ${error}`);
   });
 });
