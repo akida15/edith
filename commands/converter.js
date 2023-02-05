b65408485c29ac8eed0f96af6a94be48894f1a47
akida15 <96668437+akida15@users.noreply.github.com>
2023-02-05 22:35:52 +0100

    Update converter.js

--- commands/converter.js
+++ commands/converter.js
@@ -1,13 +1,3 @@
-/**
- Copyright (C) 2022.
- Licensed under the  GPL-3.0 License;
- You may not use this file except in compliance with the License.
- It is supplied in the hope that it may be useful.
- * @project_name : Secktor-Md
- * @author : SamPandey001 <https://github.com/SamPandey001>
- * @description : Secktor,A Multi-functional whatsapp bot.
- * @version 0.0.6
- **/
 
 const axios = require('axios')
 const { sck1, tiny, fancytext, listall,cmd } = require('../lib/')
@@ -19,9 +9,6 @@ const { exec } = require('child_process')
     //---------------------------------------------------------------------------
 cmd({
             pattern: "كتابة",
-            desc: "",
-            category: "",
-            use: '',
             filename: __filename
         },
         async(Void, citel, text) => {
@@ -29,9 +16,6 @@ cmd({
                 let text = tiny(
                     "تعديل الكتابة\n\nمثال: .كتابة 32 jiraya\n\n"
                 );
-                listall("jiraya").forEach((txt, num) => {
-                    text += `${(num += 1)} ${txt}\n`;
-                });
                 return await citel.reply(text);
             }
 
@@ -43,9 +27,6 @@ cmd({
     //---------------------------------------------------------------------------
 cmd({
             pattern: "اختصار",
-            desc: "",
-            category: "",
-            use: '',
             filename: __filename
         },
         async(Void, citel, text) => {
