ca77198bfb07a4fafc4a9b31087fb301d82bca45
akida15 <96668437+akida15@users.noreply.github.com>
2023-02-08 22:45:25 +0100

    Update rank.js

--- commands/rank.js
+++ commands/rank.js
@@ -1,76 +1,101 @@
- const { sck, sck1,cmd, jsonformat, botpic, TelegraPh, RandomXP, Config, tlang, warndb, sleep,getAdmin,getBuffer, prefix } = require('../lib')
- const moment = require("moment-timezone");
- const fs = require('fs-extra')
- const Levels = require("discord-xp");
- const canvacord = require("canvacord");
- const { Sticker, createSticker, StickerTypes } = require("wa-sticker-formatter");
- //---------------------------------------------------------------------------
- cmd({
-    pattern: "جيرايا",
+require('../../../handler/MessageHandler')
+const canvacord=require('canvacord')
+const {fetchBuffer}=require('../../../lib/Function')
+const Secktor = require('../lib')
+Secktor.cmd({
+    
+    pattern: "rank",
+    desc: "Sends rank card of user.",
+    category: "group",
+    filename: __filename,
 },
 async(Void, citel, text) => {
+    const userq = await Levels.fetch(citel.sender, "RandomXP");
+    const lvpoints = userq.level;
+    var role = "GOD✨";
+    if (lvpoints <= 2) {
+        var role = "🏳Citizen";
+    } else if (lvpoints <= 4) {
+        var role = "👼Baby Wizard";
+    } else if (lvpoints <= 6) {
+        var role = "🧙‍♀️Wizard";
+    } else if (lvpoints <= 8) {
+        var role = "🧙‍♂️Wizard Lord";
+    } else if (lvpoints <= 10) {
+        var role = "🧚🏻Baby Mage";
+    } else if (lvpoints <= 12) {
+        var role = "🧜Mage";
+    } else if (lvpoints <= 14) {
+        var role = "🧜‍♂️Master of Mage";
+    } else if (lvpoints <= 16) {
+        var role = "🌬Child of Nobel";
+    } else if (lvpoints <= 18) {
+        var role = "❄Nobel";
+    } else if (lvpoints <= 20) {
+        var role = "⚡Speed of Elite";
+    } else if (lvpoints <= 22) {
+        var role = "🎭Elite";
+    } else if (lvpoints <= 24) {
+        var role = "🥇Ace I";
+    } else if (lvpoints <= 26) {
+        var role = "🥈Ace II";
+    } else if (lvpoints <= 28) {
+        var role = "🥉Ace Master";
+    } else if (lvpoints <= 30) {
+        var role = "🎖Ace Dominator";
+    } else if (lvpoints <= 32) {
+        var role = "🏅Ace Elite";
+    } else if (lvpoints <= 34) {
+        var role = "🏆Ace Supreme";
+    } else if (lvpoints <= 36) {
+        var role = "💍Supreme I";
+    } else if (lvpoints <= 38) {
+        var role = "💎Supreme Ii";
+    } else if (lvpoints <= 40) {
+        var role = "🔮Supreme Master";
+    } else if (lvpoints <= 42) {
+        var role = "🛡Legend III";
+    } else if (lvpoints <= 44) {
+        var role = "🏹Legend II";
+    } else if (lvpoints <= 46) {
+        var role = "⚔Legend";
+    } else if (lvpoints <= 55) {
+        var role = "🐉Immortal";
+    }
     let disc = citel.sender.substring(3, 7);
-    var role = 'جيرايا'
     let textr = '';
-    textr += `
-⧉ اللقب : جيرايا
+    if (pushName) {
+        textr += `*${pushName}#${disc}'s* Exp\n\n`
+       } else {
+           textr += `*${m.sender}#${disc}'s* Exp\n\n`
+       }
+       textr += `*🎯️XP*: ${userq.xp} / ${Levels.xpFor(userq.level + 1)}\n*❤️Level*: ${userq.level}\n*🔮️Role*: ${role}`
 
-⧉ المنصب : مؤسس
+       try {
+               ppuser = await client.profilePictureUrl(m.sender, 'image')
+           } catch {
+               pppuser = 'https://www.linkpicture.com/q/IMG-20220118-WA0387.png'
+               ppuser=await fetchBuffer(pppuser)
 
-⧉ عدد مسابقات : 56 
+           }
+           const randomHexs = `#${(Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0')}`
+           const randomHex = `#${(Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0')}`
+           const randomHexz = `#${(Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0')}`
 
-⧉ الفوز : 10
-
-⧉ البنك : 627k 
-
-⧉ رابط : wa.me/+34612538080
-
-يتم تجديد البروفايل كل اسبوع وقيمته 1 مليون بيلي`;
-
-    try {
-        ppuser = 'https://i.ibb.co/jMwhNS2/th.jpg';
-    } catch {
-        ppuser = 'https://i.ibb.co/qNt1Hfx/Whats-App-Image-2023-01-29-at-17-00-05.jpg';
-    }
-
-        const randomHexs = `#${(Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0')}`
-        const randomHex = `#${(Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0')}`
-        const imageLinks = [
-            "https://i.ibb.co/XSWV1kx/1.jpg",
-            "https://i.ibb.co/CPmZrVR/2.jpg",
-            "https://i.ibb.co/NLb7bG2/3.jpg",
-            "https://i.ibb.co/3mrhcv5/4.jpg",
-            "https://i.ibb.co/WPnprpL/5.jpg",
-            "https://i.ibb.co/12xNXhM/6.jpg",
-            "https://i.ibb.co/rFRcS6m/7.jpg",
-            "https://i.ibb.co/PcBK1cp/8.jpg",
-            "https://i.ibb.co/brfCPKB/9.jpg",
-          ];
-          const randomImageLink = imageLinks[Math.floor(Math.random() * imageLinks.length)];
-          
-
-    const rank = new canvacord.Rank()
-        .setAvatar(ppuser)
-        .setLevel(10)
-        .setLevelColor(randomHex, randomHex)
-        .setCurrentXP(50)
-        .setStatus("online")
-        .setBackground("IMAGE", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJQ2FaU2C-dSC-6OlY14wM_7hWajwD3x41cA&usqp=CAU")
-        .setOverlay(randomHex, 100, false)
-        .setRequiredXP(100)
-        .setProgressBar(randomHexs, "COLOR")
-        .setBackground("IMAGE", randomImageLink)
-        .setRank(0, role, false)
-        .setUsername("Jiraya")
-        .setDiscriminator(disc);
-    rank.build()
-        .then(async(data) => {
-            Void.sendMessage(citel.chat, {
-                image: data,
-                caption: textr,
-            }, {
-                quoted: citel,
+                       const rank = new canvacord.Rank()
+               .setAvatar(ppuser)
+               .setLevel(55)
+               .setLevelColor(randomHexs, randomHex)
+               .setCurrentXP(73)
+               .setOverlay(randomHex, 100, false)
+               .setRequiredXP(6)
+               .setProgressBar(randomHexs, 'COLOR')
+               .setRank(0, role, false)
+               .setBackground('COLOR', randomHexz)
+               .setUsername(pushName)
+               .setDiscriminator(disc)
+           rank.build().then(async(data)=>{
+               client.sendMessage(m.from,{image:data,caption:textr},{quoted:m})
             });
-        });
-    }
+}
 )
