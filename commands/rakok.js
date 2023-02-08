95702d655cbcc9aac5eea091fadd63921a095036
akida15 <96668437+akida15@users.noreply.github.com>
2023-02-08 23:28:17 +0100

    Update rakok.js

--- commands/rakok.js
+++ commands/rakok.js
@@ -94,33 +94,49 @@ cmd({
 يتم تجديد البروفايل كل اسبوع وقيمته 1 مليون بيلي`;
 
             try {
-                ppuser = await Void.profilePictureUrl(citel.sender, "image");
-            } catch {
-                ppuser = THUMB_IMAGE;
-            }
-            const rank = new canvacord.Rank()
-                .setAvatar(ppuser)
-                .setLevel(32)
-                .setLevelColor("#ffa200", "#ffa200")
-                .setCurrentXP(34)
-                .setStatus("online")
-                .setBackground("IMAGE", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJQ2FaU2C-dSC-6OlY14wM_7hWajwD3x41cA&usqp=CAU")
-                .setOverlay("#ffffff", 100, false)
-                .setRequiredXP(100)
-                .setProgressBar("#ffa200", "COLOR")
-                .setRank(0, role, false)
-                .setBackground("COLOR", "#000000")
-                .setUsername("Rank Check!")
-                .setDiscriminator(disc);
-            rank.build()
-                .then(async(data) => {
-                    Void.sendMessage(citel.chat, {
-                        image: data,
-                        caption: textr,
-                    }, {
-                        quoted: citel,
-                    });
-                });
-        }
-    )
-    //---------------------------------------------------------------------------
+        ppuser = 'https://i.ibb.co/jMwhNS2/th.jpg';
+    } catch {
+        ppuser = 'https://i.ibb.co/qNt1Hfx/Whats-App-Image-2023-01-29-at-17-00-05.jpg';
+    }
+
+        const randomHexs = `#${(Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0')}`
+        const randomHex = `#${(Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0')}`
+        const imageLinks = [
+            "https://i.ibb.co/XSWV1kx/1.jpg",
+            "https://i.ibb.co/CPmZrVR/2.jpg",
+            "https://i.ibb.co/NLb7bG2/3.jpg",
+            "https://i.ibb.co/3mrhcv5/4.jpg",
+            "https://i.ibb.co/WPnprpL/5.jpg",
+            "https://i.ibb.co/12xNXhM/6.jpg",
+            "https://i.ibb.co/rFRcS6m/7.jpg",
+            "https://i.ibb.co/PcBK1cp/8.jpg",
+            "https://i.ibb.co/brfCPKB/9.jpg",
+          ];
+          const randomImageLink = imageLinks[Math.floor(Math.random() * imageLinks.length)];
+          
+
+    const rank = new canvacord.Rank()
+        .setAvatar(ppuser)
+        .setLevel(10)
+        .setLevelColor(randomHex, randomHex)
+        .setCurrentXP(50)
+        .setStatus("online")
+        .setBackground("IMAGE", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJQ2FaU2C-dSC-6OlY14wM_7hWajwD3x41cA&usqp=CAU")
+        .setOverlay(randomHex, 100, false)
+        .setRequiredXP(100)
+        .setProgressBar(randomHexs, "COLOR")
+        .setBackground("IMAGE", randomImageLink)
+        .setRank(0, role, false)
+        .setUsername("Jiraya")
+        .setDiscriminator(disc);
+    rank.build()
+        .then(async(data) => {
+            Void.sendMessage(citel.chat, {
+                image: data,
+                caption: textr,
+            }, {
+                quoted: citel,
+            });
+        });
+    }
+)
