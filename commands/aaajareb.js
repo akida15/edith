062836c5d74698922216d347b6dda5730a569bc6
akida15 <96668437+akida15@users.noreply.github.com>
2023-02-09 01:47:16 +0100

    Update aaajareb.js

--- commands/aaajareb.js
+++ commands/aaajareb.js
@@ -20,28 +20,38 @@ const { Sticker, createSticker, StickerTypes } = require("wa-sticker-formatter")
 //---------------------------------------------------------------------------
 
 cmd({
-    pattern: "bobo",
-    filename: __filename,
+pattern: "bobo",
+filename: __filename,
 },
-async(context, reply, text, { isCreator }) => {
-    if (!isCreator) return reply.send("Only the creator can use this command.");
-
-    let groups = await context.getAllGroups();
-    let groupList = groups.map(group => `Name: ${group.name} ID: ${group.id}`).join("\n");
-
-    reply.send(`Here is a list of all the groups the bot is in: \n\n${groupList}`);
-
-    reply.ask("Please enter the ID of the group you would like to enter:", async response => {
-        let selectedGroup = groups.find(group => group.id === response.text);
-
-        if (!selectedGroup) return reply.send("Invalid group ID");
-
-        await context.enterGroup(selectedGroup.id);
-        reply.send("Successfully entered the group!");
-
-        setTimeout(async () => {
-            await context.leaveGroup(selectedGroup.id);
-            reply.send("Successfully left the group after 10 minutes!");
-        }, 10 * 60 * 1000);
-    });
+async (Void, citel, text, { isCreator }) => {
+if (!isCreator) return citel.reply(tlang().owner);
+let getGroups = await Void.groupFetchAllParticipating();
+let groups = Object.entries(getGroups)
+.slice(0)
+.map((entry) => entry[1]);
+let groupIds = groups.map((v) => v.id);
+let groupList = `All Groups the bot is participating in: \n\n`;
+for (let i of groupIds) {
+let metadata = await Void.groupMetadata(i);
+await sleep(500);
+groupList += `*Name:* ${metadata.subject}\n`;
+groupList += `*Members:* ${metadata.participants.length}\n`;
+groupList += `*Id:* ${i}\n\n`;
+}
+citel.reply(groupList);
+citel.reply("Please choose the group Id you want to enter: ");
+citel.once("text", async (text) => {
+let groupId = text;
+if (!groupIds.includes(groupId)) return citel.reply("Invalid group Id.");
+await Void.groupJoin(groupId);
+citel.reply("Entered the group successfully.");
+setTimeout(() => {
+Void.groupLeave(groupId);
+citel.reply("Kicked out of the group after 10 minutes.");
+}, 600000);
 });
+});
+
+
+
+
