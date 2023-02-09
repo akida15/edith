ffd7617126e1e2bff5da713a6f9d7eac21569207
akida15 <96668437+akida15@users.noreply.github.com>
2023-02-09 02:43:02 +0100

    Update aaajareb.js

--- commands/aaajareb.js
+++ commands/aaajareb.js
@@ -14,44 +14,38 @@ const moment = require("moment-timezone");
 const fs = require('fs-extra')
 const Levels = require("discord-xp");
 const canvacord = require("canvacord");
+const eco = require('discord-mongoose-economy')
+const ty = eco.connect(mongodb);
 const { Sticker, createSticker, StickerTypes } = require("wa-sticker-formatter");
 //---------------------------------------------------------------------------
 
 //---------------------------------------------------------------------------
 
 cmd({
-pattern: "bobo",
+pattern: "ضف",
+filename: __filename,
+},
+async(Void, citel, text,{ isCreator }) => {
+      const secktor = "xp"
+     const groupAdmins = await getAdmin(Void, citel)
+    const isAdmins = citel.isGroup ? groupAdmins.includes(citel.sender) : false;
+    if (!isAdmins) return citel.reply(tlang().admin);
+     let users = citel.mentionedJid ? citel.mentionedJid : citel.msg.contextInfo.participant || false;
+     if(!users) return citel.reply('منشن من تبغى تضيف له/م')
+     users.forEach(async (user) => {
+       let member = await mongoeconomy.setXp(user, citel.chat.id, parseInt(text.split(' ')[0]));
+     });
+    return await Void.sendMessage(citel.chat,{text: `تم ضفت ${parseInt(text.split(' ')[0])} xp ل ${users.length} `,mentions:users},{quoted:citel})
+}
+)
+
+cmd({
+pattern: "xp",
 filename: __filename,
 },
 async (Void, citel, text, { isCreator }) => {
-if (!isCreator) return citel.reply(tlang().owner);
-let getGroups = await Void.groupFetchAllParticipating();
-let groups = Object.entries(getGroups)
-.slice(0)
-.map((entry) => entry[1]);
-let groupIds = groups.map((v) => v.id);
-let groupList = `All Groups the bot is participating in: \n\n`;
-for (let i of groupIds) {
-let metadata = await Void.groupMetadata(i);
-await sleep(500);
-groupList += `*Name:* ${metadata.subject}\n`;
-groupList += `*Members:* ${metadata.participants.length}\n`;
-groupList += `*Id:* ${i}\n\n`;
-}
-citel.reply(groupList);
-citel.reply("Please choose the group Id you want to enter: ");
-citel.once("text", async (text) => {
-let groupId = text;
-if (!groupIds.includes(groupId)) return citel.reply("Invalid group Id.");
-await Void.groupJoin(groupId);
-citel.reply("Entered the group successfully.");
-setTimeout(() => {
-Void.groupLeave(groupId);
-citel.reply("Kicked out of the group after 10 minutes.");
-}, 600000);
+let mention = citel.mentions.members.first() ? citel.mentions.members.first() : citel.member;
+let member = await mongoeconomy.fetchMember(mention.id, citel.chat.id);
+if (!member) return citel.reply("لم تكن تمتلك أي نقاط أو مستوى بعد...")
+citel.reply(`لدي ${member.xp} نقطة وانت على مستوى ${member.level}.`)
 });
-});
-
-
-
-
