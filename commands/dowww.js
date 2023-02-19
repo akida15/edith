900914eb6223bc9ac06fcc000dd5457e6bbf515a
akida15 <96668437+akida15@users.noreply.github.com>
2023-02-19 01:24:24 +0100

    Update dowww.js

--- commands/dowww.js
+++ commands/dowww.js
@@ -8,54 +8,29 @@ var dlsize = 1000 // 1000mb
     //---------------------------------------------------------------------------
 
 cmd({
-            pattern: "pint",
-            desc: "Downloads image from pinterest.",
-            category: "downloader",
-            filename: __filename,
-            use: '<text|image name>',
-        },
-        async(Void, citel, text) => {
-            if (!text) return reply("What picture are you looking for?") && Void.sendMessage(citel.chat, {
-                react: {
-                    text: '❌',
-                    key: citel.key
-                }
-            })
-            try {
-                anu = await pinterest(text)
-                result = anu[Math.floor(Math.random() * anu.length)]
-                let buttons = [{
-                        buttonId: `${prefix}pint ${text}`,
-                        buttonText: {
-                            displayText: 'Next Image🎀'
-                        },
-                        type: 1
-                    }
-
-                ]
-                let buttonMessage = {
-                    image: {
-                        url: result
-                    },
-                    caption: ` `,
-                    footer: tlang().footer,
-                    buttons: buttons,
-                    headerType: 4,
-                    contextInfo: {
-                        externalAdReply: {
-                            title: `Here it is✨`,
-                            body: `${Config.ownername}`,
-                            thumbnail: log0,
-                            mediaType: 2,
-                            mediaUrl: ``,
-                            sourceUrl: ``
-                        }
-                    }
-                }
-                return Void.sendMessage(citel.chat, buttonMessage, {
-                    quoted: citel
-                })
-            } catch (e) {
-                console.log(e)
+    pattern: "pint",
+    filename: __filename,
+}, async(Void, citel, text) => {
+    if (!text) {
+        return reply("What picture are you looking for?") && Void.sendMessage(citel.chat, {
+            react: {
+                text: '❌',
+                key: citel.key
             }
-        })
+        });
+    }
+    try {
+        const anu = await pinterest(text);
+        const result = anu[Math.floor(Math.random() * anu.length)];
+        const message = {
+            image: {
+                url: result
+            },
+            caption: "Here's the picture you requested!",
+        };
+        return Void.sendMessage(citel.chat, message, { quoted: citel });
+    } catch (e) {
+        console.log(e);
+    }
+});
+
