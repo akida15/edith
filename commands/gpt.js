bc4b245905c59a9fdadbaae2bb373a326fa734e0
akida15 <96668437+akida15@users.noreply.github.com>
2023-02-21 02:46:17 +0100

    Update gpt.js

--- commands/gpt.js
+++ commands/gpt.js
@@ -2,6 +2,30 @@ const axios = require('axios')
 const { sck1, tiny, fancytext, listall,cmd } = require('../lib/')
 const fs = require('fs-extra');
 const { exec } = require('child_process')
+
+cmd({
+    pattern: "توصيات",
+    fromMe: true,
+    onlyGroup: true,
+    desc: "يعطيك توصيات لأفضل الأنميات",
+    usage: "/توصيات",
+    },
+    async (message, match) => {
+        try {
+            const response = await axios.get('https://api.jikan.moe/v3/top/anime/1/bypopularity');
+            const animeList = response.data.top.slice(0, 10);
+            let result = "تفضل، إليك بعض توصياتي لأفضل 10 أنميات:\n\n";
+            animeList.forEach((anime, index) => {
+                result += `${index + 1}. ${anime.title}\n`;
+            });
+            await message.reply(result);
+        } catch (error) {
+            console.log(error);
+            await message.reply("حدث خطأ أثناء جلب التوصيات، يرجى المحاولة مرة أخرى لاحقًا.");
+        }
+    }
+);
+
 cmd({
             pattern: "أنمي",
             filename: __filename
