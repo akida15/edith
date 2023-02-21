d6f7e996953342825cebc9bc84e97fed38cd7b9a
akida15 <96668437+akida15@users.noreply.github.com>
2023-02-21 03:14:24 +0100

    Update gpt.js

--- commands/gpt.js
+++ commands/gpt.js
@@ -4,9 +4,62 @@ const { Sticker, createSticker, StickerTypes } = require("wa-sticker-formatter")
  const fs = require('fs')
  const axios = require('axios')
   //---------------------------------------------------------------------------
+ 
+ const { createCanvas, loadImage } = require("wa-image-tools");
+const { Sticker, createSticker, StickerTypes } = require("wa-sticker-formatter");
 
 cmd({
-  pattern: "random (\\d+)",
+  pattern: "onepiece",
+  filename: __filename,
+},
+async (Void, citel) => {
+  try {
+    const response = await axios.get("https://onepiece.fandom.com/api.php?action=query&format=json&list=random&rnnamespace=0&rnlimit=1");
+    const characterName = response.data.query.random[0].title.replace(" ", "_");
+    const imageUrl = `https://vignette.wikia.nocookie.net/onepiece/images/4/4d/${characterName}.png/revision/latest?cb=${new Date().getTime()}`;
+
+    const sticker = await createSticker(imageUrl, {
+      author: 'ChatGPT',
+      pack: 'One Piece Stickers',
+      type: StickerTypes.FULL,
+    });
+    await citel.sendMessage(citel.jid, sticker, Sticker, { quoted: citel.data });
+  } catch (error) {
+    console.error(error);
+    await citel.reply("Sorry, something went wrong while generating the One Piece character sticker :(");
+  }
+});
+
+
+cmd({
+  pattern: "zoro",
+  filename: __filename,
+},
+async (Void, citel) => {
+  try {
+    const image = await loadImage("https://i.imgur.com/DjgYHTy.png");
+    const canvas = createCanvas(512, 512);
+    const ctx = canvas.getContext("2d");
+    ctx.drawImage(image, 0, 0, 512, 512);
+    ctx.font = "bold 50px Arial";
+    ctx.fillStyle = "white";
+    ctx.fillText("Zoro", 200, 100);
+    const sticker = await createSticker(canvas.toBuffer(), {
+      pack: "One Piece Stickers",
+      author: "ChatGPT",
+      type: StickerTypes.FULL,
+      categories: [StickerTypes.FULL],
+    });
+    await citel.sendMessage(citel.jid, { url: sticker }, Sticker, { pack: "One Piece Stickers", author: "ChatGPT" });
+  } catch (error) {
+    console.error(error);
+    await citel.reply("Sorry, something went wrong while creating the Zoro sticker :(");
+  }
+});
+
+
+cmd({
+  pattern: "random",
   filename: __filename,
 },
 async (match, event) => {
@@ -14,3 +67,19 @@ async (match, event) => {
   const randomNumber = Math.floor(Math.random() * max) + 1
   await event.reply(`Your random number is: ${randomNumber}`)
 })
+
+cmd({
+  pattern: "cat",
+  filename: __filename,
+},
+async (Void, citel) => {
+  try {
+    const response = await axios.get("https://api.thecatapi.com/v1/images/search");
+    const imageUrl = response.data[0].url;
+    await citel.sendMessage(citel.jid, { url: imageUrl }, Sticker, { pack: "Cat Stickers", author: "ChatGPT" });
+  } catch (error) {
+    console.error(error);
+    await citel.reply("Sorry, something went wrong while fetching the cat image :(");
+  }
+});
+
