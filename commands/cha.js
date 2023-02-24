abb81b70b0bf9d736cdf8b224a8568d73054695e
akida15 <96668437+akida15@users.noreply.github.com>
2023-02-24 02:39:52 +0100

    Update cha.js

--- commands/cha.js
+++ commands/cha.js
@@ -1,5 +1,5 @@
 const fetch = require('node-fetch')
-const {fetchJson,cmd, tlang } = require('../lib')
+const { fetchJson, cmd, tlang } = require('../lib')
 
 cmd({
   pattern: "chatgpt",
@@ -7,14 +7,15 @@ cmd({
   desc: "Chat with GPT AI",
   usage: "chatgpt <text>",
   vars: true
-}, async (Void, citel, text) => {
+}, async (client, message, args) => {
+  const { text } = args
   if (!text) throw 'Please provide text to chat with GPT AI';
   try {
     let tiores = await fetchJson(`https://api.lolhuman.xyz/api/openai?apikey=BrunoSobrino&text=${text}&user=user-unique-id`)
     let hasil = tlang(tiores.result)
-    await Void.reply(citel.chat, hasil, citel.id)
+    await client.reply(message.chat, hasil, message.id)
   } catch (error) {
     console.error(error);
-    await Void.reply(citel.chat, 'Sorry, something went wrong while chatting with GPT AI :(', citel.id);
+    await client.reply(message.chat, 'Sorry, something went wrong while chatting with GPT AI :(', message.id);
   }
 });
